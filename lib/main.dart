bb64c374f48df88eddf44e7811847e13e0528de1
caster25 <teerapst1236@gmail.com>
2024-08-24 22:24:45 +0700

    test firebase pass first

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:dorm_app/screen/index.dart';
-import 'package:dorm_app/screen/test/testfirebase.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
+void main() async {
+  // Ensure that Flutter services are initialized before calling Firebase
+  WidgetsFlutterBinding.ensureInitialized();
 
-void main() {
+  // Initialize Firebase with FirebaseOptions
+  await Firebase.initializeApp(
+      options: const FirebaseOptions(
+    apiKey: 'AIzaSyDA49g2xSyXOmG5SweyId42GnIRiUhNPEE',
+    appId: '1:1014382826581:android:8640792bf386f56a8170dc',
+    messagingSenderId: 'G-T3QSM1C2CH',
+    projectId: 'accommoease-6ebe0',
+    storageBucket: 'accommoease-6ebe0.appspot.com',
+  ));
+
   runApp(const MyApp());
 }
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -35,7 +48,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: FirebaseTestScreen(),
+      body: const IndexScreen(),
       // body: const IndexScreen(),
     );
   }
@@ -49,4 +62,3 @@ class NevigationDrawer extends StatelessWidget {
     return const Placeholder();
   }
 }
-
