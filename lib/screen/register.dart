34d3c4765e13a896503e91c0ce908fae18fda14d
caster25 <teerapst1236@gmail.com>
2024-08-25 10:05:03 +0700

    1

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -24,6 +24,7 @@ class RegisterForm extends StatefulWidget {
   const RegisterForm({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _RegisterFormState createState() => _RegisterFormState();
 }
 
