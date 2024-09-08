43076c119027bd8b3f7dacdcc5d7a640ee98fded
wawwaw <jutarat.sop@ku.th>
2024-09-08 12:36:41 +0700

    0

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dorm_app/screen/index.dart';
+import 'package:dorm_app/screen/login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: IndexScreen(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
